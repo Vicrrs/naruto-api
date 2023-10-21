27ab792800f52b987ef803ea3ea4ce2b816ae6c1
Vicrrs <enricofermi2019@gmail.com>
2023-10-21 03:28:15 -0400

    Definindo as rotas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('characters', CharacterController::class);
+Route::resource('jutsus', JutsuController::class);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
