88d982b12ea7843c165156674e0c3d211d434753
Vicrrs <enricofermi2019@gmail.com>
2023-10-21 03:50:53 -0400

    Definindo o relacionamento no modelo

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -8,8 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     use HasFactory;
+
     public function jutsus()
     {
         return $this->belongsToMany(Jutsu::class);
     }
+
+    public function clan()
+    {
+        return $this->belongsTo(Clan::class);    
+    }
+
 }
