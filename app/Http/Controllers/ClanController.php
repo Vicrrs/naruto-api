04bd1dea24d4041bbd6c70ebc29fc6215c0dddbf
Vicrrs <enricofermi2019@gmail.com>
2023-10-21 12:44:09 -0400

    clan controller

--- app/Http/Controllers/ClanController.php
+++ app/Http/Controllers/ClanController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Clan;    
 use Illuminate\Http\Request;
 
 class ClanController extends Controller
 {
-    //
+    // Lista de todos os clãs
+    public function index()
+    {
+        $clans = Clan::all();
+        return response()->json($clans);
+    }
+
+    // Adicionando clã
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullabel|string',
+            'symbol_url'=>'nullabel|string'
+        ]);
+        $clan = Clan::create($request->all());
+        return response()->json($clan, 201);
+    }
+
+    // Retornando cla conforme o id
+    public function show(Clan $clan)
+    {
+        return response() -> json($clan);
+    }
+
 }
