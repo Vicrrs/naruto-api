241be63980956df49ab29195e8f3d5dd43ecc189
Vicrrs <enricofermi2019@gmail.com>
2023-10-21 02:43:46 -0400

    Definir Relacionamentos nos Modelos

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     use HasFactory;
+    public function jutsus()
+    {
+        return $this->belongsToMany(Jutsu::class);
+    }
 }
