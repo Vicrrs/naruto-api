4a7bab9d1db4e6daa6ec93ab906d226d468754c4
Vicrrs <enricofermi2019@gmail.com>
2023-10-21 14:07:41 -0400

    Adicionando Rotas para os clãs

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('characters', CharacterController::class);
 Route::resource('jutsus', JutsuController::class);
+Route::resource('clans', ClanController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
